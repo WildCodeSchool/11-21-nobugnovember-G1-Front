5e0f5485365f93b420188f1721881739bac6c8a0
Thymus <thymus@pop-os.localdomain>
2022-01-06 11:12:55 +0100

    utilisation du parametre apiKey dans l'url d'appel à l'api

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
   return (
     <div className='App'>
       {
-        function apiCall = (titleType, genre, apiKey) => {
+        function apiCall (titleType,genre,apiKey){
           const requestOptions = {
             method: 'GET',
             redirect: 'follow'
           };
           return (
-            fetch('https://imdb-api.com/API/AdvancedSearch/k_70cu350y?title_type=${titleType}&genres=${genre}&count=100', requestOptions)
+            fetch('https://imdb-api.com/API/AdvancedSearch/${apiKey}?title_type=${titleType}&genres=${genre}&count=100', requestOptions)
               .then(response => response.text())
               .then(result => console.log(result))
               .catch(error => console.log('error', error))
