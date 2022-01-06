5ed4546360fa03e75d1520cd1bd3deae358c0306
Thymus <thymus@pop-os.localdomain>
2022-01-06 12:30:16 +0100

    ajout d'une fonction handleChange associer au click des emojis et creation d'un tableau de correspondance

--- src/components/Emojis.js
+++ src/components/Emojis.js
@@ -10,36 +10,47 @@ import surprise from '../assets/surprise.png'
 import './Emojis.css'
 
 const Emojis = () => {
+  const handleChange = () =>{
+    console.log ("coucou");
+    const correspondanceTab=[ {emotion:"Emoji joyeux",filter:"comedy"},
+                              {emotion:"Emoji pleure",filter:"drama"},
+                              {emotion:"Emoji surprise",filter:"fantasy,sci_fi"},
+                              {emotion:"Emoji amoureux",filter:"romance"},
+                              {emotion:"Emoji colere",filter:"action,war"},
+                              {emotion:"Emoji peur",filter:"horror"},
+                              {emotion:"Emoji auberg",filter:""}
+    ]
+  }
   return (
     <div className='emojibarre'>
       <div className='container'>
-        <NavLink to='/Catalogue' className=''>
-          <img className='emojiimg' src={joyeux} alt='Emoji joyeux'></img>
+        <NavLink  to='/Catalogue' className=''>
+          <img onClick={handleChange} className='emojiimg' src={joyeux} alt='Emoji joyeux'></img>
         </NavLink>
         <figcaption>Joyeux</figcaption>
       </div>
       <div className='container'>
-        <img className='emojiimg' src={pleure} alt='Emoji pleure'></img>
+        <img onClick={handleChange} className='emojiimg' src={pleure} alt='Emoji pleure'></img>
         <figcaption>Triste</figcaption>
       </div>
       <div className='container'>
-        <img className='emojiimg' src={surprise} alt='Emoji surprise'></img>
+        <img onClick={handleChange} className='emojiimg' src={surprise} alt='Emoji surprise'></img>
         <figcaption>Surpris</figcaption>
       </div>
       <div className='container'>
-        <img className='emojiimg' src={amour} alt='Emoji amoureux'></img>
+        <img onClick={handleChange} className='emojiimg' src={amour} alt='Emoji amoureux'></img>
         <figcaption>Amoureux</figcaption>
       </div>
       <div className='container'>
-        <img className='emojiimg' src={colere} alt='Emoji colere'></img>
+        <img onClick={handleChange} className='emojiimg' src={colere} alt='Emoji colere'></img>
         <figcaption>Enervé</figcaption>
       </div>
       <div className='container'>
-        <img className='emojiimg' src={peur} alt='Emoji peur'></img>
+        <img onClick={handleChange} className='emojiimg' src={peur} alt='Emoji peur'></img>
         <figcaption>Effrayé</figcaption>
       </div>
       <div className='container'>
-        <img className='emojiimg' src={aubergine} alt='Emoji auberg'></img>
+        <img onClick={handleChange} className='emojiimg' src={aubergine} alt='Emoji auberg'></img>
         <figcaption>Coquin</figcaption>
       </div>
     </div>
