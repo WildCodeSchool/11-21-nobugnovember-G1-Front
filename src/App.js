c5ad7021d40197a338a9fb174b59fced1cc4fade
GRATADE Jean-François <gratade.jeanfrancois@gmail.com>
2022-01-07 16:00:26 +0100

    Mise à jour du footer
    Logos noir avec changement de couleur en hover (jaune)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Films from './screens/Films'
 import MiniJeux from './screens/MiniJeux'
 import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer';
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
 
 function App() {
   return (
