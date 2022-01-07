9d96ee313fc9b260d28fa650bd489fc090a96894
Mickaseg <mickael_seguin@hotmail.fr>
2022-01-07 17:48:05 +0100

    card props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,16 @@
-import affiche from '../assets/affichetest.jpg'
 import './Cards.css'
-const Cards = (props) => {
+
+const Cards = props => {
   return (
     <div className='cards'>
-      <img src={affiche} className='poster' alt='poster'></img>
+      <img src={props.poster} className='poster' alt='poster'></img>
       <div className='caption'>
         <h3>
           {props.title}
-
+          {` `}
+          {props.description}
         </h3>
       </div>
-      {/* <span>2018</span> */}
     </div>
   )
 }
--- src/screens/Catalogue.js
+++ src/screens/Catalogue.js
@@ -1,30 +1,36 @@
 import Cards from '../components/Cards'
 import Header from '../components/Header'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Catalogue.css'
 import apiCall from '../scripts/api'
 
 const Catalogue = () => {
-  const apiKey='k_70cu350y';
-  const typeTitle='movies&tv_series';
-  const [filtreApi,setFiltreApi]=useState(localStorage.getItem('maSelection'));
-  const [resultat,setResultat]= useState([]);
-      useEffect(()=> {
-        apiCall(typeTitle,filtreApi,apiKey).then(res=> setResultat(res.results))
-      },[])
-  //console.log(apiCall('movies','drama','k_70cu350y'))
+  const apiKey = 'k_gcprl00i'
+  const typeTitle = 'movies&tv_series'
+  const [filtreApi, setFiltreApi] = useState(
+    localStorage.getItem('maSelection')
+  )
+  const [resultat, setResultat] = useState([])
+  useEffect(() => {
+    apiCall(typeTitle, filtreApi, apiKey).then(res => setResultat(res.results))
+  }, [])
+
   return (
     <div className='catalogPage'>
-
       <div className='catalogContainer'>
         <Header />
         <div className='movie-grid'>
-          { resultat.map( element => <Cards title={element.title} />) }
-
+          {resultat.map(element => (
+            <Cards
+              title={element.title}
+              poster={element.image}
+              description={element.description}
+            />
+          ))}
         </div>
       </div>
     </div>
- )
+  )
 }
 
 export default Catalogue
