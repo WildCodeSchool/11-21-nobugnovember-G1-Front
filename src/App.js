b53709c926963c82d9b4e26323a4ca1fbaed865b
MUNSCH Thomas <92970500+MunschThomas@users.noreply.github.com>
2022-01-07 16:06:57 +0100

    Update App.js
    deleted import reactDom

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Films from './screens/Films'
 import MiniJeux from './screens/MiniJeux'
 import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer';
-import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
