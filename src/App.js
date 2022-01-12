e4164783552c42a33e72d2b34a2a5ec39bb8ed0d
MUNSCH <thomas-munsch_student2021@wilder.school>
2022-01-12 23:12:24 +0100

    Added subroute /Catalogue/FicheFilm

--- src/App.js
+++ src/App.js
@@ -9,17 +9,87 @@ import { Routes, Route } from 'react-router-dom'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
 import { useState } from 'react'
 function App() {
-  const [emojiSelected,setEmojiSelected]=useState('')
+  const [emojiSelected, setEmojiSelected] = useState('')
   const [resultat, setResultat] = useState([])
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Home emojiSelected={emojiSelected} setEmojiSelected={setEmojiSelected}/>} />
-        <Route path='/Catalogue' element={<Catalogue emojiSelected={emojiSelected} setEmojiSelected={setEmojiSelected} resultat={resultat} setResultat={setResultat}/>} />
-        <Route path='/Films' element={<Films emojiSelected={emojiSelected} setEmojiSelected={setEmojiSelected} resultat={resultat} setResultat={setResultat}/>} />
-        <Route path='/Series' element={<Series  emojiSelected={emojiSelected} setEmojiSelected={setEmojiSelected} resultat={resultat} setResultat={setResultat}/>} />
-        <Route path='/MiniJeux' element={<MiniJeux emojiSelected={emojiSelected} setEmojiSelected={setEmojiSelected} resultat={resultat} setResultat={setResultat}/>} />
-        <Route path='/FicheFilm' element={<CardFilm emojiSelected={emojiSelected} setEmojiSelected={setEmojiSelected} resultat={resultat} setResultat={setResultat}/>} />
+        <Route
+          path='/'
+          element={
+            <Home
+              emojiSelected={emojiSelected}
+              setEmojiSelected={setEmojiSelected}
+            />
+          }
+        />
+        <Route
+          path='/Catalogue'
+          element={
+            <Catalogue
+              emojiSelected={emojiSelected}
+              setEmojiSelected={setEmojiSelected}
+              resultat={resultat}
+              setResultat={setResultat}
+            />
+          }
+        >
+          <Route
+            path='/Catalogue/FicheFilm'
+            element={
+              <CardFilm
+                emojiSelected={emojiSelected}
+                setEmojiSelected={setEmojiSelected}
+                resultat={resultat}
+                setResultat={setResultat}
+              />
+            }
+          />
+        </Route>
+        <Route
+          path='/Films'
+          element={
+            <Films
+              emojiSelected={emojiSelected}
+              setEmojiSelected={setEmojiSelected}
+              resultat={resultat}
+              setResultat={setResultat}
+            />
+          }
+        />
+        <Route
+          path='/Series'
+          element={
+            <Series
+              emojiSelected={emojiSelected}
+              setEmojiSelected={setEmojiSelected}
+              resultat={resultat}
+              setResultat={setResultat}
+            />
+          }
+        />
+        <Route
+          path='/MiniJeux'
+          element={
+            <MiniJeux
+              emojiSelected={emojiSelected}
+              setEmojiSelected={setEmojiSelected}
+              resultat={resultat}
+              setResultat={setResultat}
+            />
+          }
+        />
+        <Route
+          path='/FicheFilm'
+          element={
+            <CardFilm
+              emojiSelected={emojiSelected}
+              setEmojiSelected={setEmojiSelected}
+              resultat={resultat}
+              setResultat={setResultat}
+            />
+          }
+        />
       </Routes>
     </div>
   )
--- src/screens/Catalogue.js
+++ src/screens/Catalogue.js
@@ -5,6 +5,7 @@ import './Catalogue.css'
 import Footer from '../components/Footer'
 import Loading from '../components/Loading'
 import axios from 'axios'
+import { Link, Outlet } from 'react-router-dom'
 
 const Catalogue = props => {
   const [isLoading, setIsLoading] = useState(false)
@@ -12,28 +13,43 @@ const Catalogue = props => {
 
   const titleType = 'movies&tv_series'
 
-  useEffect(()=>{
-    setIsLoading(true);
-      axios.get(`https://imdb-api.com/API/AdvancedSearch/${apiKey}?title_type=${titleType}&genres=${props.emojiSelected.correspondance}&count=100`)
-      .then((response)=>response.data)
-      .then((data)=>{props.setResultat(data.results);
-      setIsLoading(false);});
-  },[props.emojiSelected.correspondance])
+  useEffect(() => {
+    setIsLoading(true)
+    axios
+      .get(
+        `https://imdb-api.com/API/AdvancedSearch/${apiKey}?title_type=${titleType}&genres=${props.emojiSelected.correspondance}&count=100`
+      )
+      .then(response => response.data)
+      .then(data => {
+        props.setResultat(data.results)
+        setIsLoading(false)
+      })
+  }, [props.emojiSelected.correspondance])
   return (
     <div className='catalogPage'>
       <div className='catalogContainer'>
-        <Header className='headerband' emojiSelected={props.emojiSelected} setEmojiSelected={props.setEmojiSelected}/>
-        {isLoading?<Loading />:
-        <div className='movie-grid'>
-          {props.resultat.map(element => (
-            <Cards key={element.key}
-              title={element.title}
-              poster={element.image}
-              description={element.description}
-            />
-          ))}
-        </div>}
-
+        <Outlet />
+        <Header
+          className='headerband'
+          emojiSelected={props.emojiSelected}
+          setEmojiSelected={props.setEmojiSelected}
+        />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className='movie-grid'>
+            {props.resultat.map((element, index) => (
+              <Link to='/Catalogue/FicheFilm' key={index}>
+                <Cards
+                  key={element.key}
+                  title={element.title}
+                  poster={element.image}
+                  description={element.description}
+                />
+              </Link>
+            ))}
+          </div>
+        )}
 
         <Footer />
       </div>
