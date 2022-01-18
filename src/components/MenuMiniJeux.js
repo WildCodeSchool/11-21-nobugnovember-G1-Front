14ec71f73d3c598be43d5879923f59e9901030bd
Marsaleix Romain <msx.coach@gmail.com>
2022-01-18 23:23:54 +0100

    Last save Emoji changer vertical et horizon

--- src/components/MenuMiniJeux.js
+++ src/components/MenuMiniJeux.js
@@ -3,11 +3,11 @@ import { useState } from 'react'
 
 const MenuMiniJeux = props => {
   const [choixMiniJeux, setChoixMinijeux] = useState()
-  
+
   const handleChoice = () => {
     setChoixMinijeux(event.target.value)
   }
-  
+
   const handleClick = () => {
     const saisie = document.querySelector('.inputPseudo')
     props.setPlayerName(saisie.value)
@@ -20,19 +20,25 @@ const MenuMiniJeux = props => {
   return (
     <div className='page-menu-minijeux'>
       <h2>Bienvenue dans la section Mini-Jeux</h2>
-
       <p>
+        Derrière ce jeu, 
         Cette partie a été créée afin de vérifier tes connaissances
-        cinématographiques ainsi que ta mémoire auditive ! Pour faire simple, tu auras la possibilité de participer à des mini-jeux en rapport avec ta sélection HUMEUR DU JOUR.
-        <ul >
-          <li className="li-minijeux">
-            Le premier, un quizz contenant des questions sur des films & séries.
-          </li>
-          <li className="li-minijeux">
-            Le second, un blind contenant des musiques et des extraits de films
-            & séries.
-          </li>
-        </ul>
+        cinématographiques ainsi que ta mémoire auditive ! Pour faire simple,
+        ces mini-jeux que nous{' '}
+      </p>
+
+      <h2>Le Remote-Quizz</h2>
+      <p>
+        Ce premier jeu, comme son nom l'indique, est un quizz contenant des
+        questions traitant de films & séries provenant d'une base de données de
+        produits liés à l'humeur que tu as préalablement séléctionné.
+      </p>
+
+      <h2>Le Blind-test </h2>
+      <p>
+        {' '}
+        Le second, un blind contenant des musiques et des extraits de films &
+        séries.
       </p>
       <label>Veuillez saisir un pseudo</label>
       <input className='inputPseudo' onChange={handleChange} />
